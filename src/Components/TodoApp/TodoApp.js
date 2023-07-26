b1c59141c580f234518597b7e5ee2d1c50486a2c
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-26 22:14:38 +0500

    Do Some Changes

--- src/Components/TodoApp/TodoApp.js
+++ src/Components/TodoApp/TodoApp.js
@@ -1,11 +1,24 @@
 import './Todo.css'
+import { useState } from 'react'
+
 export default function TodoApp() {
+  const [InputText, setInputText] = useState("");
+
+  const inputData = (e) => {
+    setInputText(e.target.value);
+    console.log(InputText);
+  };
+  
+
+
   return (
     <div>
-        <center>
-          <h1>Todo App</h1>
-        </center>
-      
+      <center>
+        <h1>Todo App</h1>
+        <input type="text" onChange={inputData} />
+        <button >Add</button>
+        <p>{InputText}</p>
+      </center>
     </div>
-  )
+  );
 }
