c4389aa4c4ebc55b841eea7a404bb8f02853fff4
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-26 22:17:56 +0500

    Sir Project Started

--- src/Components/TodoApp/TodoApp.js
+++ src/Components/TodoApp/TodoApp.js
@@ -1,24 +1,9 @@
-import './Todo.css'
-import { useState } from 'react'
+import React from 'react'
 
-export default function TodoApp() {
-  const [InputText, setInputText] = useState("");
-
-  const inputData = (e) => {
-    setInputText(e.target.value);
-    console.log(InputText);
-  };
-  
-
-
+export default function Students() {
   return (
     <div>
-      <center>
-        <h1>Todo App</h1>
-        <input type="text" onChange={inputData} />
-        <button >Add</button>
-        <p>{InputText}</p>
-      </center>
+      
     </div>
-  );
+  )
 }
