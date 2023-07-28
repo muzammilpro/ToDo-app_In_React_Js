75b258e8e5eb14bd0d035641a307052fc0b7a580
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-28 15:41:44 +0500

    Do Some Changes

--- src/Components/TodoApp/Data.js
+++ src/Components/TodoApp/Data.js
@@ -12,6 +12,12 @@ export const data = [
     class: "Web and App"
    },
    {
+    name: "Shehraz",
+    batch: "06" ,
+    roll: "99",
+    class: "Web and App"
+   },
+   {
     name:"Rana Sab" ,
     batch:"06",
     roll: "100",
