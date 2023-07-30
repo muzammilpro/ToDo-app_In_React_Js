ea1ff375419f2380ab730596c7069c375087fd57
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-30 17:06:59 +0500

    DO Some Changes

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -32,39 +32,36 @@ export default function Todo() {
     return (
         <div>
             <center>
-
                 <div className='upperSection'>
-                    <h1>Add New Task</h1>
+                    <h1 className='heading'>Welcome To The Muzammil Todo App</h1>
+                    <h1 className='heading'>Add New Task</h1>
                     <div className='upperSection1'>
-                        <input type="text" value={text} onChange={currentText} placeholder='Add new task' />
-                        <button onClick={toDisplay}>Add Task</button>
+                        <input className='inputArea' type="text" value={text} onChange={currentText} placeholder='Add new task' />
+                        <button className='Hbtn' onClick={toDisplay}>Add Task</button>
                     </div>
                 </div>
-
-
-
             </center>
+
             <br />
+
             <center>
-
-
                 <div className='lowerParent'>
-                    <h1>Task List</h1>
+                    <h1 className='heading'>Task List</h1>
                     {inputs.map((input, index) => (
                         <div >
                             <div key={index} className='taskList' >
-                                <p >{input}</p>
+                                <div className='pTag'><p>{input}</p></div>
                                 <div className='btnCon'>
-                                    <button onClick={() => deleteHandler(index)}>Delete</button>
-                                    <button onClick={() => editHandler(index)} className='editBtn'>Edit</button>
+                                    <button className='btnbtn' onClick={() => deleteHandler(index)}>Delete</button>
+                                    <button onClick={() => editHandler(index)} className=' btnbtn'>Edit</button>
                                 </div>
                             </div>
                             <br />
                         </div>
                     ))}
-
                 </div>
             </center>
         </div>
     );
 }
+
